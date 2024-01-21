e5235c5236a09407982cb048ac1ba11824d312e7
Rohit Shinde <143794245+rohitshinde08@users.noreply.github.com>
2024-01-21 19:03:51 +0530

    Delete SEM_VI directory

--- SEM_VI/WT_2/Assignment_1/q1.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    if (isset($_SESSION['cnt']))
-       $_SESSION['cnt']+=1;
-    else
-       $_SESSION['cnt']=1;
-    echo "Web Page Access ".$_SESSION['cnt']," Times";
-?>
